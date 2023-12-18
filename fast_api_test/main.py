243cace05f522142ec801a897365fc0adb1d2640
unknown <voroncov2031@gmail.com>
2023-12-18 18:31:09 +0200

    first

---
+++ fast_api_test/main.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from fastapi import FastAPI
+from routers.items import router as items_router
+
+app = FastAPI()
+
+
+app.include_router(
+    router=items_router,
+    prefix='/items'
+)
