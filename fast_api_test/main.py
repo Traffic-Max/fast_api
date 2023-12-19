b7a08e89be70695600e7bd3bff822b1ef287dc55
Max <a111@111s-MacBook-Pro.local>
2023-12-19 08:15:14 +0200

    third

--- fast_api_test/main.py
+++ fast_api_test/main.py
@@ -3,6 +3,7 @@ from model import core
 from model.database import engine
 from fastapi import FastAPI
 from routers.items import router as items_router
+from routers.users import router as users_router
 
 
 core.Base.metadata.create_all(bind=engine)
@@ -13,4 +14,10 @@ app = FastAPI()
 app.include_router(
     router=items_router,
     prefix='/items'
+)
+
+
+app.include_router(
+    router=users_router,
+    prefix='/users'
 )
